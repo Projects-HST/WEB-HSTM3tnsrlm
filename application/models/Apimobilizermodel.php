3a7a137169652ffd17d5fc54aa8124cb30ba23ac
kamalhappysanz <kamal.happysanz@gmail.com>
2019-12-20 17:30:03 +0530

    aadhaar card number added

--- application/models/Apimobilizermodel.php
+++ application/models/Apimobilizermodel.php
@@ -439,7 +439,7 @@ class Apimobilizermodel extends CI_Model {
 //#################### List Students ####################//
 	public function listStudents($user_id)
 	{
-		 	$student_query = "SELECT id,name,sex,mobile,email,enrollment,status,student_pic FROM edu_student_prospects WHERE created_by  ='$user_id'";
+		 	$student_query = "SELECT id,name,sex,mobile,email,enrollment,status,student_pic,aadhaar_card_number FROM edu_student_prospects WHERE created_by  ='$user_id'";
 			$student_res = $this->db->query($student_query);
 			$query_result= $student_res->result();
 			$student_count = $student_res->num_rows();
@@ -463,6 +463,7 @@ class Apimobilizermodel extends CI_Model {
             "enrollment"=>$rows_list->enrollment,
             "status"=>$rows_list->status,
             "student_pic"=>$student_pic,
+            "aadhaar_card_number"=>$rows_list->aadhaar_card_number,
 
 
           );
@@ -477,7 +478,7 @@ class Apimobilizermodel extends CI_Model {
 //#################### List Students ####################//
 	public function listStudentsStatus($user_id,$status)
 	{
-		 	$student_query = "SELECT id,name,sex,mobile,email,enrollment,status,student_pic FROM edu_student_prospects WHERE created_by = '$user_id' AND status = '$status'";
+		 	$student_query = "SELECT id,name,sex,mobile,email,enrollment,status,student_pic,aadhaar_card_number FROM edu_student_prospects WHERE created_by = '$user_id' AND status = '$status'";
 			$student_res = $this->db->query($student_query);
 			$query_result= $student_res->result();
 			$student_count = $student_res->num_rows();
@@ -502,7 +503,7 @@ class Apimobilizermodel extends CI_Model {
             "enrollment"=>$rows_list->enrollment,
             "status"=>$rows_list->status,
             "student_pic"=>$student_pic,
-
+            "aadhaar_card_number"=>$rows_list->aadhaar_card_number,
 
           );
         }
