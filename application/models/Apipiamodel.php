c822fff5a2c05df6ccc8ee351ba5258d03f90cb1
kamalhappysanz <kamal.happysanz@gmail.com>
2020-02-17 11:36:32 +0530

    Committed

--- application/models/Apipiamodel.php
+++ application/models/Apipiamodel.php
@@ -786,7 +786,8 @@ class Apipiamodel extends CI_Model {
 			}else{
 			        $student_query = "INSERT INTO edu_student_prospects (pia_id,have_aadhaar_card, aadhaar_card_number, name, sex, dob, age, nationality, religion, community_class, community, father_name, mother_name, mobile, sec_mobile, email, state, city, address, mother_tongue, disability, blood_group, admission_date, admission_location, admission_latitude, admission_longitude, preferred_trade, preferred_timing, last_institute, last_studied, qualified_promotion, transfer_certificate, status, created_by, created_at)
               VALUES ('$pia_id','$have_aadhaar_card', '$aadhaar_card_number', '$name', '$sex', '$dob', '$age', '$nationality', '$religion', '$community_class', '$community', '$father_name', '$mother_name', '$mobile', '$sec_mobile', '$email', '$state',
-                '$city', '$address', '$mother_tongue', '$disability', '$blood_group', '$admission_date', '$admission_location', '$admission_latitude', '$admission_longitude', '$preferred_trade', '$preferred_timing', '$last_institute', '$last_studied', '$qualified_promotion', '$transfer_certificate', '$status', '$pia_id', now())";
+                '$city', '$address', '$mother_tongue', '$disability', '$blood_group', '$admission_date', '$admission_location', '$admission_latitude', '$admission_longitude', '$preferred_trade', '$preferred_timing', '$last_institute',
+                '$last_studied', '$qualified_promotion', '$transfer_certificate', '$status', '$pia_id', NOW())";
 	                $student_res = $this->db->query($student_query);
                     $student_id = $this->db->insert_id();
 
