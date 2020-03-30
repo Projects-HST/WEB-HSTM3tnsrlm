0bc56e04c09b2812f9c8e0529c08299e37ed509d
Maran <maran.happysanz@gmail.com>
2020-03-30 19:49:42 +0530

    Update consolidated_report.php

--- application/views/pia/staff/consolidated_report.php
+++ application/views/pia/staff/consolidated_report.php
@@ -4,7 +4,6 @@ foreach($mobilizer_details as $mobi){
 } 
 $month = $consolidate_report['month_name'];
 	$sMonth = date("m", strtotime("$month"));
-	$month_id = $date['month'];
 ?>
 <!doctype html>
 <html class="no-js" lang="">
@@ -89,7 +88,7 @@ $month = $consolidate_report['month_name'];
 							</tr>
 							</table>
 						</div>
-						<div class="col-lg-2 col-md-3 col-sm-3 col-xs-12"><a class="pull-right btn btn-primary btn-xs" href="<?php echo base_url(); ?>staff/consolidate_generateXls/<?php echo base64_encode($mobi_id*98765);?>/<?php echo $consolidate_report['year'];?>/<?php echo $month_id; ?>"><i class="fa fa-file-excel-o"></i> Export Data</a></div>
+						<div class="col-lg-2 col-md-3 col-sm-3 col-xs-12"><a class="pull-right btn btn-primary btn-xs" href="<?php echo base_url(); ?>staff/consolidate_generateXls/<?php echo base64_encode($mobi_id*98765);?>/<?php echo $consolidate_report['year'];?>/<?php echo $sMonth; ?>"><i class="fa fa-file-excel-o"></i> Export Data</a></div>
 						
 				   </div>
 		</div>
