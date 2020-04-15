5c5e50ad0237f1cab4861052f0034782cc1e4388
kamalhappysanz <kamal.happysanz@gmail.com>
2020-04-15 16:25:43 +0530

    km added

--- application/models/Apipiamodel.php
+++ application/models/Apipiamodel.php
@@ -1483,7 +1483,32 @@ function prospects_document($prospect_id){
         }
 
 
-        $response=array("status"=>"success","msg"=>"attedance details","field_work_images"=>$field_work_details_report,"attedance_details"=>$response_attedance);
+
+        $km_query="SELECT et.user_id,Round(sum((6371 * ACOS(
+                COS( RADIANS(to_lat) )
+              * COS( RADIANS( user_lat ) )
+              * COS( RADIANS( user_long ) - RADIANS(to_long) )
+              + SIN( RADIANS(to_lat) )
+              * SIN( RADIANS( user_lat ) )
+            ) )),2) AS km
+        FROM edu_tracking_details as et left join edu_users as eu on eu.user_id=et.user_id WHERE eu.user_id='$mobilizer_id' and DATE(created_at)='$rows_details->attendance_date'";
+        $re_km_query=$this->db->query($km_query);
+        $result_km=$re_km_query->result();
+        foreach($result_km as $rows_km){}
+          $km_details=$rows_km->km;
+          if($km_details='NULL'){
+            $res_km=array("status"=>"error","msg"=>"No KM details found");
+          }else{
+            $details=array(
+              "user_id"=>$rows_km->user_id,
+              "km"=>$rows_km->km
+            );
+            $res_km=array("status"=>"success","msg"=>"KM details found","km_data"=>$details);
+          }
+
+
+
+        $response=array("status"=>"success","msg"=>"attedance details","field_work_images"=>$field_work_details_report,"attedance_details"=>$response_attedance,"km_data_details"=>$res_km);
         return $response;
       }
 
